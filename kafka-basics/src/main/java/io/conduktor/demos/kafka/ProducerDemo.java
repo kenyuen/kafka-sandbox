8de91de0582851950f11bc4291588974b8615975
Ken Yuen <kenneth.x.yuen@gmail.com>
2024-12-26 06:39:33 -0600

    Initial commit

---
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
@@ -0,0 +1,45 @@
+package io.conduktor.demos.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("Hello Kafka Producer!");
+
+        // create Producer properties
+        Properties properties = new Properties();
+
+        // connect to local host (note that this is hosted under docker on my machine)
+        properties.setProperty("bootstrap.servers", "127.0.0.1:19092");
+
+        // connect to cdk-gateway - another day...
+
+        // set producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // create a Producer record
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello world");
+
+        // send data
+        producer.send(producerRecord);
+
+        // tell producer to send all data and block until done -- synchronous
+        producer.flush();
+
+        // flush and close producer
+        producer.close();
+
+    }
+}

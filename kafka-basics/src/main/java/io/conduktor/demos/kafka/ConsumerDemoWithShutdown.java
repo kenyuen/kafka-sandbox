d4321002b32d8bbc5d0273bd6e54fae45f25af7b
Ken Yuen <kenneth.x.yuen@gmail.com>
2024-12-27 13:40:23 -0600

    Added a Consumer Demo with Shutdown hook to gracefully shutdown.

---
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ConsumerDemoWithShutdown.java
@@ -0,0 +1,87 @@
+package io.conduktor.demos.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerDemoWithShutdown {
+    private static final Logger log = LoggerFactory.getLogger(ConsumerDemoWithShutdown.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("I am Kafka Consumer!");
+
+        String groupId = "my-java-application";
+        String topic = "demo_java";
+
+        // create Producer properties
+        Properties properties = new Properties();
+
+        // connect to local host (note that this is hosted under docker on my machine)
+        properties.setProperty("bootstrap.servers", "127.0.0.1:19092");
+
+        // connect to cdk-gateway - another day...
+
+        // set producer properties
+        properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("value.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("group.id", groupId);
+        properties.setProperty("auto.offset.reset", "earliest");
+
+        // create the Consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        // get a reference to the shutdown hook
+        final Thread mainThread = Thread.currentThread();
+
+        // add a shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                log.info("Caught shutdown hook");
+                consumer.wakeup();
+
+                // join the main thread to allow the execution of the code in the main thread
+                try {
+                    mainThread.join();
+                } catch (InterruptedException e) {
+                    log.error("Exception while joining main thread", e);
+                }
+
+            }
+        });
+
+        try {
+            // subscribe to our topic(s)
+            consumer.subscribe(Arrays.asList(topic));
+
+            // poll for data
+            while (true) {
+                log.info("Polling for data...");
+
+                ConsumerRecords<String, String> records =
+                        consumer.poll(Duration.ofMillis(100)); // new in Kafka 2.0.0
+
+                for (ConsumerRecord<String, String> record: records) {
+                    log.info("Key: " + record.key() + " | Value: " + record.value());
+                    log.info("Partition: " + record.partition() + " | Offset: " + record.offset());
+                }
+            }
+        } catch (WakeupException e) {
+            log.info("Consumer is starting to shutdown");
+        } catch (Exception e) {
+            log.error("Unexpected exception in the consumer", e);
+        } finally {
+            consumer.close(); // close the consumer, this will also commit the offsets.
+            log.info("The consumer is now gracefully shutdown");
+        }
+
+
+    }
+}
